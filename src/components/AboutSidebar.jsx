3715acdf07687c960f68ab27a5707f9eff14a9d2
Milos Zekovic <milos.zekov@gmail.com>
2019-04-28 00:48:55 +0200

    Light theme dev

--- src/components/AboutSidebar.jsx
+++ src/components/AboutSidebar.jsx
@@ -7,7 +7,7 @@ class AboutSidebar extends Component {
         <article className="sidebar">
             <AboutMilos />
             <h4>Skills and services</h4>
-            <p>HTML5 - CSS / SASS - BEM<br />JavaScript - jQuery - jQuery Mobile<br />ReactJS - VueJS - Gulp<br />GIT / SVN - UX / UI<br />Web Design - Responsive Design<br />Atomic Design Methodology<br />Material Design<br />Progressive Enhancement<br />Pixel Perfect Layouts<br />From PSD to HTML - SEO<br />WordPress - Joomla - Magento</p>
+            <p>HTML5 - CSS / SASS - BEM<br />JavaScript - jQuery - jQuery Mobile<br />ReactJS - VueJS - Gulp<br />GIT / SVN - UX / UI - CSS Grid<br />Web Design - Responsive Design<br />Atomic Design - Material Design<br />Progressive Enhancement<br />Pixel Perfect Layouts<br />From PSD to HTML - SEO<br />WordPress - Joomla - Magento</p>
             <h4>Dev and design tools</h4>
             <p>Visual Studio / VS Code<br />Sketch<br />Adobe CC<br />Adobe XD</p>
         </article>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,12 +4,16 @@ import { Link, NavLink } from 'react-router-dom'
 import Footer from './Footer'
 import IconHome from '../assets/img/ui/icon-home.svg'
 import IconHomeActive from '../assets/img/ui/icon-home-active.svg'
+import IconHomeLightActive from '../assets/img/ui/icon-home-light-active.svg'
 import IconAbout from '../assets/img/ui/icon-about.svg'
 import IconAboutActive from '../assets/img/ui/icon-about-active.svg'
+import IconAboutLightActive from '../assets/img/ui/icon-about-light-active.svg'
 import IconPortfolio from '../assets/img/ui/icon-portfolio.svg'
 import IconPortfolioActive from '../assets/img/ui/icon-portfolio-active.svg'
+import IconPortfolioLightActive from '../assets/img/ui/icon-portfolio-light-active.svg'
 import IconContact from '../assets/img/ui/icon-contact.svg'
 import IconContactActive from '../assets/img/ui/icon-contact-active.svg'
+import IconContactLightActive from '../assets/img/ui/icon-contact-light-active.svg'
 
 class NavBar extends Component {
   render() {
@@ -28,24 +32,28 @@ class NavBar extends Component {
               <NavLink to="/" exact activeClassName="active">
                 <div className="nav--button"><img src={IconHome} alt="Miloš Zeković - Front-end developer / UX/UI designer" /></div>
                 <div className="nav--button-active"><img src={IconHomeActive} alt="Miloš Zeković - Front-end developer / UX/UI designer" /></div>
+                <div className="nav--button-light-active"><img src={IconHomeLightActive} alt="Miloš Zeković - Front-end developer / UX/UI designer" /></div>
               </NavLink>
             </li>
             <li>
               <NavLink to="/about" exact activeClassName="active">
                 <div className="nav--button"><img src={IconAbout} alt="Miloš Zeković - About me" /></div>
                 <div className="nav--button-active"><img src={IconAboutActive} alt="Miloš Zeković - About me" /></div>
+                <div className="nav--button-light-active"><img src={IconAboutLightActive} alt="Miloš Zeković - About me" /></div>
               </NavLink>
             </li>
             <li>
               <NavLink to="/portfolio" exact activeClassName="active">
                 <div className="nav--button"><img src={IconPortfolio} alt="Miloš Zeković - Portfolio" /></div>
                 <div className="nav--button-active"><img src={IconPortfolioActive} alt="Miloš Zeković - Portfolio" /></div>
+                <div className="nav--button-light-active"><img src={IconPortfolioLightActive} alt="Miloš Zeković - Portfolio" /></div>
               </NavLink>
             </li>
             <li>
               <NavLink to="/contact" exact activeClassName="active">
                 <div className="nav--button"><img src={IconContact} alt="Miloš Zeković - Contact" /></div>
                 <div className="nav--button-active"><img src={IconContactActive} alt="Miloš Zeković - Contact" /></div>
+                <div className="nav--button-light-active"><img src={IconContactLightActive} alt="Miloš Zeković - Contact" /></div>
               </NavLink>
             </li>
           </ul>
