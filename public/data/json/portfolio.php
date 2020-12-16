4c16a5af50bb4ec34db7e183813e4650a1ce6913
Milos Zekovic <milos.zekov@gmail.com>
2020-12-16 23:46:25 +0100

    webp images

--- public/data/json/portfolio.php
+++ public/data/json/portfolio.php
@@ -11,7 +11,7 @@
         "year": "2020",
         "url": "https://mikus.rs/",
         "urlName": "Mikus.rs",
-        "img": "https://mze.ninja/data/img/mikus.jpg"
+        "img": "https://mze.ninja/data/img/mikus.webp"
       },
       {
         "id": 2,
@@ -21,7 +21,7 @@
         "year": "2020",
         "url": "https://www.casinoroom.com/",
         "urlName": "Casinoroom.com",
-        "img": "https://mze.ninja/data/img/casinoroom.jpg"
+        "img": "https://mze.ninja/data/img/casinoroom.webp"
       },
       {
         "id": 3,
@@ -31,7 +31,7 @@
         "year": "2020",
         "url": "https://www.ellmountgaming.com/",
         "urlName": "Ellmountgaming.com",
-        "img": "https://mze.ninja/data/img/ellmount.jpg"
+        "img": "https://mze.ninja/data/img/ellmount.webp"
       },
       {
         "id": 4,
@@ -41,7 +41,7 @@
         "year": 2019,
         "url": "https://github.com/miloszekovic/frontend-for-tv-screens-example",
         "urlName": "SEB TV project on GitHub",
-        "img": "https://mze.ninja/data/img/seb-tv.jpg"
+        "img": "https://mze.ninja/data/img/seb-tv.webp"
       },
       {
         "id": 5,
@@ -51,7 +51,7 @@
         "year": 2018,
         "url": "https://www.atos-vrata.rs/",
         "urlName": "Atos-vrata.rs",
-        "img": "https://mze.ninja/data/img/atos-vrata.jpg"
+        "img": "https://mze.ninja/data/img/atos-vrata.webp"
       },
       {
         "id": 6,
@@ -61,7 +61,7 @@
         "year": 2018,
         "url": "https://www.evitbe.com/",
         "urlName": "Evitbe.com",
-        "img": "https://mze.ninja/data/img/evitbe.jpg"
+        "img": "https://mze.ninja/data/img/evitbe.webp"
       },
       {
         "id": 7,
@@ -71,7 +71,7 @@
         "year": 2017,
         "url": "https://www.vpbg.rs/",
         "urlName": "Vpbg.rs",
-        "img": "https://mze.ninja/data/img/vpbg.jpg"
+        "img": "https://mze.ninja/data/img/vpbg.webp"
       },
       {
         "id": 8,
@@ -81,7 +81,7 @@
         "year": 2017,
         "url": "https://www.moldovabridge.com/",
         "urlName": "Moldovabridge.com",
-        "img": "https://mze.ninja/data/img/moldovabridge.jpg"
+        "img": "https://mze.ninja/data/img/moldovabridge.webp"
       },
       {
         "id": 9,
@@ -91,7 +91,7 @@
         "year": 2016,
         "url": "https://moj.pausal.rs/#/login/",
         "urlName": "moj.Pausal.rs",
-        "img": "https://mze.ninja/data/img/pausal.jpg"
+        "img": "https://mze.ninja/data/img/pausal.webp"
       },
       {
         "id": 10,
@@ -101,7 +101,7 @@
         "year": 2016,
         "url": "https://www.beyoubefree.rs/",
         "urlName": "Beyoubefree.rs",
-        "img": "https://mze.ninja/data/img/be-you.jpg"
+        "img": "https://mze.ninja/data/img/be-you.webp"
       },
       {
         "id": 11,
@@ -111,7 +111,7 @@
         "year": 2016,
         "url": "https://www.zla-zla.com/zlashtek/",
         "urlName": "Zla-zla.com",
-        "img": "https://mze.ninja/data/img/zla-zla.jpg"
+        "img": "https://mze.ninja/data/img/zla-zla.webp"
       },
       {
         "id": 12,
@@ -121,7 +121,7 @@
         "year": 2015,
         "url": "https://www.starapazova.rs/",
         "urlName": "Starapazova.rs",
-        "img": "https://mze.ninja/data/img/stara-pazova.jpg"
+        "img": "https://mze.ninja/data/img/stara-pazova.webp"
       },
       {
         "id": 13,
@@ -131,7 +131,7 @@
         "year": 2012,
         "url": "https://www.olivebox.net/",
         "urlName": "Olivebox.net",
-        "img": "https://mze.ninja/data/img/olive-box-web.jpg"
+        "img": "https://mze.ninja/data/img/olive-box-web.webp"
       },
       {
         "id": 14,
@@ -141,7 +141,7 @@
         "year": 2013,
         "url": "https://www.olivebox.net/pricing-signup/",
         "urlName": "Olivebox.net",
-        "img": "https://mze.ninja/data/img/olive-box-app.jpg"
+        "img": "https://mze.ninja/data/img/olive-box-app.webp"
       },
       {
         "id": 15,
@@ -151,7 +151,7 @@
         "year": 2014,
         "url": "https://www.svetasrbija.org.rs/",
         "urlName": "Svetasrbija.org.rs",
-        "img": "https://mze.ninja/data/img/sveta-srbija.jpg"
+        "img": "https://mze.ninja/data/img/sveta-srbija.webp"
       },
       {
         "id": 16,
@@ -161,7 +161,7 @@
         "year": 2010,
         "url": "https://www.zonaii.com/",
         "urlName": "Zonaii.com",
-        "img": "https://mze.ninja/data/img/zonaii.jpg"
+        "img": "https://mze.ninja/data/img/zonaii.webp"
       },
       {
         "id": 17,
@@ -171,7 +171,7 @@
         "year": 2009,
         "url": "https://madness.mze.ninja/",
         "urlName": "Madness.rs",
-        "img": "https://mze.ninja/data/img/madness.jpg"
+        "img": "https://mze.ninja/data/img/madness.webp"
       },
       {
         "id": 18,
@@ -181,7 +181,7 @@
         "year": 2008,
         "url": "https://vojackazvona.mze.ninja/",
         "urlName": "Vojackazvona.rs",
-        "img": "https://mze.ninja/data/img/zvona.jpg"
+        "img": "https://mze.ninja/data/img/zvona.webp"
       }
     ]';
     echo $json;
